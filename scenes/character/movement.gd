bdc2bd6a698a65bb9d8b51221414cf212bc8cc23
Andy Black <sprotex@gmail.com>
2024-11-30 10:29:46 +0100

    fix framerate for movement

--- scenes/character/movement.gd
+++ scenes/character/movement.gd
@@ -5,7 +5,7 @@ class_name Movement
 @export var body: CharacterBody2D
 @export var move_speed := 1.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+
+func _physics_process(_delta: float) -> void:
 	body.velocity = input.input_movement * move_speed
 	body.move_and_slide()
